412ea34d15587809c37d6aad5aacee872eebb38f
WallisonSantos <wallisontiagoassis206@outlook.com>
2021-11-28 11:24:13 -0300

    first commit

---
+++ src/main/java/br/com/ser/parcerias/controller/HelloController.java
@@ -0,0 +1,8 @@
+package br.com.ser.parcerias.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+    
+}
---
+++ src/test/java/br/com/ser/parcerias/ParceriasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ser.parcerias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParceriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
